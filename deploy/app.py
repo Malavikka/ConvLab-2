ea75f0391a9585417f72b353f942610bcac6a739
Malavikka <30741987+Malavikka@users.noreply.github.com>
2020-06-29 18:20:19 +0530

    Update app.py

--- deploy/app.py
+++ deploy/app.py
@@ -1,4 +1,4 @@
-#from flask_ngrok import run_with_ngrok
+from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template, request
 import convlab2
 from convlab2.nlu.jointBERT.multiwoz import BERTNLU
@@ -14,7 +14,7 @@ import numpy as np
 import torch
 
 app = Flask(__name__)
-#run_with_ngrok(app)   #starts ngrok when the app is run
+run_with_ngrok(app)   #starts ngrok when the app is run
 @app.route("/")
 def home():
     return "<h1>Running Flask on Google Colab!</h1>"
